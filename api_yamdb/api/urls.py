4821db07ae919b0f99c0e49d3f4e8e0919f865a0
Natalya Shulgina <natulishka84@gmail.com>
2022-11-30 23:46:08 +0300

    endpoint signup

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,19 +4,8 @@ from rest_framework.routers import SimpleRouter
 from .views import SignupViewSet
 
 api_v1_router = SimpleRouter()
-# api_v1_router.register(
-#     r'titles/(?P<title_id>[1-9]\d*)/reviews',
-#     ReviewsViewSet,
-#     basename='reviews'
-# )
-# api_v1_router.register(
-#     r'titles/(?P<title_id>[1-9]\d*)/reviews/(?P<review_id>[1-9]\d*)/comments',
-#     CommentsViewSet,
-#     basename='comments'
-# )
-
 
 urlpatterns = [
-    path('v1/auth/signup/', ),
-    path('v1/', include(api_v1_router.urls), SignupViewSet),
+    path('v1/auth/signup/', SignupViewSet),
+    path('v1/', include(api_v1_router.urls)),
 ]
