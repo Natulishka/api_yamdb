edf52d9e9f23cb39fb7f2c0c006aa7153a977628
Natalya Shulgina <natulishka84@gmail.com>
2022-12-01 20:40:49 +0300

    endpoint_users

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -5,7 +5,7 @@ from .models import User
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'password', 'role', 'is_superuser',
-                    'is_staff', 'is_active', 'confirmation_code')
+                    'is_staff', 'is_active')
     search_fields = ('username', 'email')
     empty_value_display = '-пусто-'
 
