da3face88956e5f179380936579948a40623f0b0
Natalya Shulgina <natulishka84@gmail.com>
2022-12-03 16:36:02 +0300

    fixed settings after merger

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,8 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig'
-    'api.apps.ApiConfig'
+    'users.apps.UsersConfig',
+    'api.apps.ApiConfig',
     'rest_framework',
     # 'rest_framework.authtoken'
     'django_filters',
